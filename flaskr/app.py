116985c93edd31bd1f9bb885d089b1257229a1a1
tansionline <2014513023@ogr.mehmetakif.edu.tr>
2020-12-13 23:22:59 +0300

    htmls, app.py updated

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,26 +1,75 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, session
 from models import db 
 from forms import LoginForm, RegisterForm
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "MemurBeySelcuk"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///home/tasn4/Desktop/github/cevrimici-kitap-galerisi/flaskr/secret.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+def __repr__(self):
+    return '<User %r>' % self.username
 
-db.init_app(app)
 
 with app.app_context():
+    db.init_app(app)
     db.create_all()
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-    loginForm = LoginForm()
+    form = LoginForm()
     
-    return render_template('login.html', loginForm = loginForm)
+    return render_template('login.html', form = form)
+
+@app.route('/register/', methods = ['GET', 'POST'])
+def register():
+    # Creating RegistrationForm class object
+    form = RegisterForm()
+
+    # Cheking that method is post and form is valid or not.
+    if request.method == 'POST' and form.validate():
+        user = User.query.filter_by(email = form.email.data).first()
+        # if all is fine, generate hashed password
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+
+        # create new user model object
+        new_user = User(
+
+            name = form.name.data, 
+
+            email = form.email.data, 
+
+            password = hashed_password,
+
+            
+            avatar = form.avatar.data) 
+
+        # saving user object into data base with hashed password
+        db.session.add(new_user)
+
+        db.session.commit()
+
+        # if registration successful, then redirecting to login Api
+        return redirect(url_for('login'))
+
+    else:
+
+        # if method is Get, than render registration form
+        return render_template('register.html', form = form)
+@app.route('/logout/')
+def logout():
+    # Removing data from session by setting logged_flag to False.
+    session['logged_in'] = False
+    # redirecting to home page
+    return redirect(url_for('home'))
+
+
+    # Creating database tables
+
 
 
 if __name__ == "__main__":
--- flaskr/forms.py
+++ flaskr/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
 
 class RegisterForm(FlaskForm):
     name = StringField('enter your name', validators=[DataRequired()])
     email = StringField('enter your email', validators=[DataRequired()])
-    password_one = StringField('your password', validators=[DataRequired()])
-    password_two = StringField('enter your password again', validators=[DataRequired()])
+    password = PasswordField('your password', validators=[DataRequired()])
+    confirm = PasswordField('enter your password again', validators=[DataRequired()])
     avatar = StringField('enter your avatar', validators=[DataRequired()])
 
 class LoginForm(FlaskForm):
