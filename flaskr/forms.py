21e1cb710f5377577828303d85a5f3f9db5da95c
tansionline <2014513023@ogr.mehmetakif.edu.tr>
2020-12-13 08:35:42 +0300

    placefolders changed

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -3,13 +3,13 @@ from wtforms import StringField
 from wtforms.validators import DataRequired
 
 class RegisterForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired()])
-    password_one = StringField('password', validators=[DataRequired()])
-    password_two = StringField('enter again password', validators=[DataRequired()])
-    avatar = StringField('avatar', validators=[DataRequired()])
+    name = StringField('enter your name', validators=[DataRequired()])
+    email = StringField('enter your email', validators=[DataRequired()])
+    password_one = StringField('your password', validators=[DataRequired()])
+    password_two = StringField('enter your password again', validators=[DataRequired()])
+    avatar = StringField('enter your avatar', validators=[DataRequired()])
 
 class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
+    email = StringField('enter your email', validators=[DataRequired()])
+    password = StringField('enter your password', validators=[DataRequired()])
 
